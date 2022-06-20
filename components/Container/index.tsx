3468a6718ef0efd7f3a7f9314670948816de62ab
Adrien <adrienlcp@gmail.com>
2022-06-20 15:22:39 +0200

    init project with css reset, variables, mixins, etc..

---
+++ components/Container/index.tsx
@@ -0,0 +1,15 @@
+// import Navbar from '../Navbar';
+import Head from "../Header";
+
+const Container = (props: any) => {
+
+  return (
+    <>
+      <Head />
+      {/* <Navbar /> */}
+      {props.children}
+    </>
+  );
+};
+
+export default Container;
