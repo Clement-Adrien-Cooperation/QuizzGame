e00ae7a642c7944a7521fbbf3b6e03273a34c49d
ClementCHA <73785703+ClementCHA@users.noreply.github.com>
2022-06-23 18:21:00 +0200

    clean files

--- pages/api/getAllUsers.ts
+++ pages/api/getAllUsers.ts
@@ -1,16 +1,14 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiResponse } from 'next';
 
 import { prisma } from '../../lib/prisma';
 
 // POST /api/user
 // Required fields in body: name, email
 export default async function getAlluser(
-  req: NextApiRequest,
   res: NextApiResponse
 ) {
   try {
     const user = await prisma.user.findMany()
-    console.log(user)
     res.status(201).json(user)
     
   } catch (error){
